a7617376a6d9f6e811c193cb75b18e171e53ff24
Friedemann Schwenzer <Friedemann.Schwenzer@posteo.de>
2025-10-01 10:24:52 +0200

    Update elements.py

--- elements.py
+++ elements.py
@@ -136,7 +136,7 @@ INTRO = [
 <a href="https://www.freitag.de/autoren/hendrik-bolz/sieg-heil-rufe-wiegten-mich-in-den-schlaf"
 target="_blank" style="color:#0000fc; text-decoration:underline;">
 Essay</a> 
-des Rappers Hendrick Bolz auf Twitter. Es beschreibt eine Jugend im Ostdeutschland der Nachwendezeit, geprägt von rechter Gewalt. Eine Generationenerfahrung.""",
+des Rappers Hendrick Bolz auf Twitter. Es beschreibt seine von rechter Gewalt geprägte Jugend im Ostdeutschland der Nachwendezeit. Eine Generationenerfahrung.""",
     """Hunderte Nutzer*innen folgten dem Aufruf Bangels und teilten ihre autobiographischen Erinnerungen unter dem Hashtag #Baseballschlägerjahre. Für meine Masterarbeit habe ich diese Tweets gesammelt und ausgewertet. In dieser Webapp mache ich die Geschichten zugänglich."""
 ]
 
